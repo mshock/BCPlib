b7892f6a17918071086cfcd3f28b69ab7a2d592d
mshock <shockleyme@gmail.com>
2012-10-01 11:12:00 -0500

    started writing constructor and accessor methods

--- BCPlib.pm
+++ BCPlib.pm
@@ -3,11 +3,24 @@
 package BCPlib;
 
 use strict;
+
+# allow functional access
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(bcp);
+
+use Carp;
 use Pod::Usage qw(pod2usage);
+use Params::Check qw(check);
 use Getopt::Std qw(getopts);
+
+# autoload global var
+our $AUTOLOAD;
 
 my %cli_args;
 getopts('hv', \%cli_args);
+
+my $verbose = $cli_args{v};
 
 # only CLI function is to print usage
 usage() unless caller;
@@ -23,10 +36,110 @@ usage() unless caller;
 #	subs
 ####################################
 
-# man sub for running bcp with opts
+# constructor
+sub new {
+	my ($class, $args_href) = @_;
+	$class = ref($class) || $class;
+	
+	# database objects are hashrefs of database info
+	my ($export_db, $import_db);
+	
+	# Params::Check template
+	my $tmpl = {
+		export_db => {	
+			default => {},
+			strict_type => 1,
+			store => \$import_db,
+		},
+		import_db => {
+			default => {},
+			strict_type => 1,
+			store => \$export_db,
+		},	
+	};
+	# verify all arguments
+	check($tmpl, $args_href, $verbose)
+		or carp "unable to parse BCPlib constructor args\n"
+		and return undef;
+	
+	# create hashref object
+	my $self = {
+		export_db => $export_db,
+		import_db => $import_db,
+	};
+	
+	# bless & return object
+	bless $self, $class;
+	return $self;
+}
+
+
+# functional interface to module
 sub bcp {
 	
 }
+
+# OO bcp in
+sub in {
+	my ($self, $args_href) = @_;
+	
+	# verify import database
+	my $tmpl = {
+		import_database => {},
+	};
+	check($tmpl, $args_href, $verbose)
+		or die "unable to parse args\n";
+	
+}
+
+# OO bcp out
+sub out {
+	
+}
+
+# OO bcp queryout
+sub queryout {
+	
+}
+
+# get/set through AUTOLOAD
+sub AUTOLOAD {
+	my $self = shift or return undef;
+	my $func = $AUTOLOAD;
+	
+	# determine get/set and which variable
+	if ($func =~ m/(\w+)_(\w+)/i) {
+		my ($op,$attr) = ($1,$2);
+		# get attribute
+		if (lc $op eq 'get') {
+			# add new sub to symbol table
+			{
+				no strict 'refs';
+				*{$AUTOLOAD} = sub {return shift->{$attr}};
+			}
+		}
+		# set attribute
+		elsif (lc $op eq 'set') {
+			{
+				no strict 'refs';
+				*{$AUTOLOAD} = sub {return shift->{$attr} = shift};
+			}
+		}
+		else {
+			carp "unsupported operation $op in AUTOLOAD\n";
+			return undef;
+		}
+		# goto the newly created method
+		goto &$AUTOLOAD;
+	}
+	else {
+		carp "unable to parse call to $func in AUTOLOAD\n";
+		return undef;
+	}	
+}
+
+# avoid calling AUTOLOAD on DESTROY
+sub DESTROY {};
 
 # print usage from pod
 sub usage {
@@ -39,9 +152,7 @@ sub usage {
 	pod2usage({ 
 		-msg => 'BCPlib Perl Module',
 		-verbose => $verbosity,
-		-exitval => $exit_val,
-			
-		
+		-exitval => $exit_val,		
 	});
 }
 
@@ -67,7 +178,7 @@ BCPlib - library of functions interfacing with BCP w/ SQL focus
 This is a simple library interface to the BCP Windows utility.
 I believe there is a need for this because Sybase(::BCP) is not included in ActiveState's Perl Package Manager (PPM)
 
-=head1 SEE ALSOI
+=head1 SEE ALSO
 
 Sybase::BCP
 http://search.cpan.org/~mewp/sybperl-2.19/BCP/BCP.pm
