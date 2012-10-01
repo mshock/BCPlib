960dae1ffd4783bfdfb2b68c10de2cbcb7be99ab
mshock <shockleyme@gmail.com>
2012-10-01 16:47:16 -0500

    added lots of parameter checking which I may just end up removing

--- BCPlib.pm
+++ BCPlib.pm
@@ -20,13 +20,10 @@ our $AUTOLOAD;
 my %cli_args;
 getopts('hv', \%cli_args);
 
-my $verbose = $cli_args{v};
+my $verbose = 0;
 
 # only CLI function is to print usage
 usage() unless caller;
-
-
-
 
 # package returns The Truth
 1;
@@ -38,11 +35,12 @@ usage() unless caller;
 
 # constructor
 sub new {
-	my ($class, $args_href) = @_;
+	my $class = shift;
 	$class = ref($class) || $class;
+	my $params = shift;
 	
 	# database objects are hashrefs of database info
-	my ($export_db, $import_db);
+	my ($export_db, $import_db, $bcp_path, $error_log);
 	
 	# Params::Check template
 	my $tmpl = {
@@ -55,10 +53,20 @@ sub new {
 			default => {},
 			strict_type => 1,
 			store => \$export_db,
-		},	
+		},
+		bcp_path => {
+			default => '',
+			strict_type => 1,
+			store => \$bcp_path,
+		},
+		error_log => {
+			default => 'bcp.errors',
+			strict_type => 1,
+			store => \$error_log,
+		},
 	};
 	# verify all arguments
-	check($tmpl, $args_href, $verbose)
+	check($tmpl, $params, $verbose)
 		or carp "unable to parse BCPlib constructor args\n"
 		and return undef;
 	
@@ -66,6 +74,9 @@ sub new {
 	my $self = {
 		export_db => $export_db,
 		import_db => $import_db,
+		bcp_path => $bcp_path,
+		error_log => $error_log,
+		last_result => undef,
 	};
 	
 	# bless & return object
@@ -76,20 +87,116 @@ sub new {
 
 # functional interface to module
 sub bcp {
+	my $params = shift;
 	
+	my ($op,$db,$bcp_path,$table,$error_log,$encoding,$query_out);
+	
+	my $tmpl = {
+		op => {
+			required => 1,
+			allow => qr/^(in|out|queryout)$/i,
+			store => \$op,
+		},
+		query => {
+			defined => 1,
+			store => \$query_out,
+		},
+		db => {
+			required => 1,
+			defined => 1,
+			store => \$db,
+		},
+		bcp_path => {
+			required => 1,
+			defined => 1,
+			store => \$bcp_path,
+		},
+		table => {
+			defined => 1,
+			store => \$table,
+		},
+		error_log => {
+			default => 'bcp.errors',
+			strict_type => 1,
+			store => \$error_log,
+		},
+		encoding => {
+			default => 'c',
+			strict_type => 1,
+			allow => qr/^[cnNw]$/,
+			store => \$encoding,
+		},
+	};
+	check($tmpl, $params, $verbose)
+		or carp "unable to parse bcp args\n"
+		and return;
+	
+	# check that there is a query for queryout
+	if ($op eq 'queryout' && !$query_out) {
+		carp "no query passed for queryout in bcp";
+		return;
+	} 
+	
+	# change bcp's first argument based on operation
+	my $bcp_arg = $op eq 'queryout' ? $query_out : "[$db->{name}].dbo.[$table]";
+	
+	# run bcp command
+	return `bcp $bcp_arg $op $bcp_path -S$db->{server} -U$db->{user} -P$db->{pwd} -e$error_log -$encoding`;
 }
 
 # OO bcp in
 sub in {
-	my ($self, $args_href) = @_;
+	my $self = shift;
+	my $params = shift;
 	
-	# verify import database
+	my ($import_db, $table, $error_log, $encoding, $bcp_path);
 	my $tmpl = {
-		import_database => {},
+		import_db => {
+			default => $self->{import_db},
+			strict_type => 1,
+			store => \$import_db,
+		},
+		table => {
+			required => 1,
+			defined => 1,
+			store => \$table,
+		},
+		error_log => {
+			default => $self->{error_log},
+			strict_type => 1,
+			store => \$error_log,
+		},
+		encoding => {
+			default => 'c',
+			strict_type => 1,
+			store => \$encoding,
+		},
+		bcp_path => {
+			default => $self->{bcp_path},
+			strict_type => 1,
+			store => \$bcp_path,
+		}
 	};
-	check($tmpl, $args_href, $verbose)
-		or die "unable to parse args\n";
 	
+	check($tmpl, $params, $verbose) 
+		or carp "unable to parse bcp in params\n"
+		and return undef;	
+	
+	# check that an import database has been added to the object
+	unless ($import_db) {
+		carp "no import database configured cannot bcp in\n";
+		return undef;
+	}
+	unless ($bcp_path) {
+		carp "no bcp path configured cannot bcp in\n";
+		return undef;
+	}
+
+	my $cmd_db = "[$self->{import_database}->{name}].dbo.[$table]";
+	
+	$self->{last_result} = `bcp $cmd_db in $self->{bcp_path} -e$error_log -$encoding`;
+	
+	return $self;
 }
 
 # OO bcp out
@@ -129,13 +236,14 @@ sub AUTOLOAD {
 			carp "unsupported operation $op in AUTOLOAD\n";
 			return undef;
 		}
-		# goto the newly created method
-		goto &$AUTOLOAD;
 	}
 	else {
 		carp "unable to parse call to $func in AUTOLOAD\n";
 		return undef;
-	}	
+	}
+	
+	# goto the newly created method
+	goto &$AUTOLOAD;
 }
 
 # avoid calling AUTOLOAD on DESTROY
